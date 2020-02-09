4a5bb00deed3ccb794c07aec3cb219731ceb70a5
Le Lan-Toson <clelantoson@hotmail.fr>
2020-02-09 22:22:01 +0100

    edit tried but failed for now

--- lib/controller.rb
+++ lib/controller.rb
@@ -14,10 +14,23 @@ class ApplicationController < Sinatra::Base
     erb :index, locals: {all_gossips: Gossip.all}
   end
 
+  #voir repo jade caillot on peut aussi utiliser params en dessous
   get '/gossips/:id' do
     id = params['id'].to_i
     Gossip.find(id)
     erb :show, locals: {gossip_find: Gossip.find(id), index: id}
+  end
+
+  get '/gossips/:id/edit/' do
+    id = params['id'].to_i
+    erb :edit, locals: {gossip: Gossip.find(id), id: id}
+  end
+
+  post '/gossips/:id/edit/' do
+    id = params['id'].to_i
+    g_to_update = Gossip.find(id)
+    g_to_update.update(id,params["gossip_author"], params["gossip_content"]).
+    redirect '/gossips/'
   end
 end
 
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -13,14 +13,14 @@ class Gossip
   end
 
   def save
-    CSV.open("db/gossip.csv", "ab") do |csv|
+    CSV.open("./db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
   end
 
   def self.all
     all_gossips = []
-    CSV.read('db/gossip.csv').each do |line|
+    CSV.read('./db/gossip.csv').each do |line|
       new_gossip = Gossip.new(line[0], line[1])
       all_gossips << new_gossip
     end
@@ -33,5 +33,43 @@ class Gossip
     puts gossip
     return gossip
   end
+
+  # ou juste écrire
+  # def self.find(id)
+  #   Gossip.all[id-1]
+  # end
+
+  def self.update(id, gossip_to_update)
+    all_gossips_updated = []
+    all_gossips = Gossip.all
+    all_gossips.with_index do |gossip, index|
+      if index == (id-1)
+        all_gossips_updated << self
+      else
+        all_gossips_updated << all_gossips[index]
+      end
+    end
+  end
+
+#premiere tentative de update
+    #   def self.update(id, author, content)
+  #   array_to_update = Gossip.all
+  #   array_to_update.map do |element|
+  #     if element == id
+  #      puts ""
+  #     end
+  #   end
+  #   puts array_to_update
+  # end 
+
+  # def self.delete(id, author, content)
+  #   array_to_update = Gossip.all
+  #   array_to_update.map do |element|
+  #     if element == id
+  #       delete_at_index(id)
+  #     end
+  #   end
+  #   puts array_to_update
+  # end 
 
 end
